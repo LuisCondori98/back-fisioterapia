7b9e48f64f197f1fea257953c72976a8300425d4
unknown <lcondori11@hotmail.com>
2026-02-14 22:49:37 -0500

    corrigiendo el mail service

--- src/controller/cita.controller.js
+++ src/controller/cita.controller.js
@@ -14,7 +14,10 @@ export const citaController = {
 
       logger.info("Generated cita")
 
-      await sendEmail(`${cita.terapeuta.correo}`,
+      await sendEmail([
+                        "lcondori11@hotmail.com",
+                        cita.terapeuta.correo
+                      ],
                       "Cita Confirmada - Centro de Fisioterapia",
                       `
                       <div style="font-family: Arial, sans-serif; background-color: #f4f6f9; padding: 20px;">
