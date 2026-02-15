31a5e16f6f8f833b5b0904abaedc812169f683f4
unknown <lcondori11@hotmail.com>
2026-02-14 19:34:12 -0500

    add isadmin contactos

--- src/router/contacto.route.js
+++ src/router/contacto.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { contactoController } from "../controller/contacto.controller.js";
+import { isAdmin } from "../helpers/jwt.js";
 
 export const contactoRouter = Router()
 
 contactoRouter.post("/", contactoController.crearContacto)
-contactoRouter.get("/", contactoController.leerContactos)
+contactoRouter.get("/", isAdmin, contactoController.leerContactos)
